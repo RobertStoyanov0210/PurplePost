046f341a216d01d55298ae22f3d09de8ba01fcc9
Robert Stoyanov <rstoianov0210@gmail.com>
2024-01-04 23:46:42 +0200

    fixing bugs

--- purplepost/app/controllers/Pages.php
+++ purplepost/app/controllers/Pages.php
@@ -10,12 +10,12 @@ class Pages extends Controller
       redirect('posts');
     }
 
-    $data = ['title' => "Welcome to my website", 'description' => 'Simple post sharing website build on custom MVC.'];
+    $data = ['title' => "Welcome to my website", 'description' => 'Simple post sharing website build on <a target="_blank" href="https://github.com/RobertStoyanov0210/robertsmvc">custom MVC</a>.'];
     $this->view('pages/index', $data);
   }
   public function about()
   {
-    $data = ['title' => "This is the default About page.", 'description' => 'App to share posts with other users.'];
+    $data = ['title' => "This is the default About page.", 'description' => ' Sign up to peek into the amazing world of user-generated content on PurplePost. It\'s where all the cool stuff happens! :)'];
     $this->view('pages/about', $data);
   }
 }
--- purplepost/app/views/pages/index.php
+++ purplepost/app/views/pages/index.php
@@ -3,6 +3,7 @@
   <div class="container text-center">
     <h1 class="display-3"><?php echo $data['title'] ?></h1>
     <p class="lead"><?php echo $data['description']; ?></p>
+    <a href="https://github.com/RobertStoyanov0210/robertsmvc"></a>
   </div>
 </div>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
